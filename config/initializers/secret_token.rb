0ef29593d99736a66a115fc49ecdeda27fa6bf4a
Ricardo Cruz <piradoiv@gmail.com>
2013-07-25 20:03:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yatapi::Application.config.secret_token = '798da431c09003f53a5424275cf3364ac492993126a0d9a9f4d1c6b9d03605459acf323f2d114e0af5844061d337c582dfb7cfd00d459c91c6d7c7d157af1cd1'
